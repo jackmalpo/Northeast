fe3e43a904f064ba0ce4881bc40d4756cbc1e857
Jack Malpasuto <jack.malpasuto@metova.com>
2016-08-01 13:56:43 -0500

    Dev Test 1

--- app/src/main/java/com/malpo/northeast/BaseApplication.java
+++ app/src/main/java/com/malpo/northeast/BaseApplication.java
@@ -12,6 +12,7 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //Dev Test 1
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
